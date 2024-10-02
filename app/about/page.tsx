0afc02e162b18ffda5dc4ebadc8aa4dfe79900bc
rekad <7062944+rekad@users.noreply.github.com>
2024-10-02 13:45:10 +0200

    update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,15 @@
 export default function About() {
     return (
         <main>
-            Kumo is a radio.
+            Kumo is a radio station.<br /><br />
+
+            A place for conversations. <br /><br />
+
+            A place for experiences to be shared and heard.<br /><br />
+
+            A place to encounter voices that are both familiar and unfamiliar.<br /><br />
+
+            <a href="https://www.mixcloud.com/KumoRadio/" target="_blank">Listen to the archives | アーカイブを聴く</a>
         </main>
     )
 }
