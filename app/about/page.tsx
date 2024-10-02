f10069c47b13dd1069b82e1c21886372263a2454
rekad <7062944+rekad@users.noreply.github.com>
2024-10-02 22:47:10 +0200

    fix about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,18 @@
 export default function About() {
     return (
-        <main>
-            Kumo is a radio station.<br /><br />
+        <main className="">
+            <div className="mt-4">
+                Kumo is a radio station.<br /><br />
 
-            A place for conversations. <br /><br />
+                A place for conversations. <br /><br />
 
-            A place for experiences to be shared and heard.<br /><br />
+                A place for experiences to be shared and heard.<br /><br />
 
-            A place to encounter voices that are both familiar and unfamiliar.<br /><br />
+                A place to encounter voices that are both familiar and unfamiliar.<br /><br />
 
-            <a href="https://www.mixcloud.com/KumoRadio/" target="_blank">Listen to the archives | アーカイブを聴く</a>
-            <a href="https://www.instagram.com/radiokumo/" target="_blank">Follow us on Instagram</a>
+                <a href="https://www.mixcloud.com/KumoRadio/" target="_blank">Listen to the archives | アーカイブを聴く</a><br/>
+                <a href="https://www.instagram.com/radiokumo/" target="_blank">Follow us on Instagram</a>
+            </div>
         </main>
     )
 }
